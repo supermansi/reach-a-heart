d49cac07fa35ba7158a927a975b7cc3a234c24dc
ksanghvi <sanghvi.ki@husky.neu.edu>
2020-02-02 10:11:46 -0500

    UI changes org

--- src/Components/Organization.js
+++ src/Components/Organization.js
@@ -15,7 +15,7 @@ class Organization extends Component {
          <div>
           <div id="OrgCard" class="card"/>
            <div class="card-body">
-             Register with our website and get donors everyday by posting requirements.
+             <h3>Register with our website and get donors everyday by posting requirements.</h3>
              <h1>Get in touch today!</h1>
              
              <Link to="/createProfile">
@@ -23,65 +23,37 @@ class Organization extends Component {
           </Link>
            </div>
            </div>
-        <div><h3>Our User Stories</h3></div>
+        <div><h3>Organization Success Stories</h3></div>
         
         <div class="container mt-4">
         <div class="row">
             <div class="col-auto mb-3">
             <div class="card" style={{width: 18+'rem'}}>
-            <img src="..." class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Pet lovers organizations</h5>
+              <p class="card-text">This website helped us to post events, requirements and campaigns. We got in touch 
+              with a lot of donors and philantropists.</p>
             </div>
             </div>
             </div>
             <div class="col-auto mb-3">
               <div class="card" style={{width: 18+'rem'}}>
-              <img src="..." class="card-img-top" alt="..."/>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">One For All, All for one</h5>
+                <p class="card-text"> It is a one-stop for getting to know what people are willing to 
+              donate in a particular area. Great website to collaborate.</p>
               </div>
               </div>
             </div>
             <div class="col-auto mb-3">
               <div class="card" style={{width: 18+'rem'}}>
-              <img src="..." class="card-img-top" alt="..."/>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">Help Society</h5>
+                <p class="card-text">It is a one-stop for getting to know what people are willing to 
+              donate in a particular area. Great website to collaborate.</p>
               </div>
               </div>
-            </div>
-            <div class="col-auto mb-3">
-              <div class="card" style={{width: 18+'rem'}}>
-              <img src="..." class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              </div>
-            </div>
-            <div class="col-auto mb-3">
-            <div class="card" style={{width: 18+'rem'}}>
-            <img src="..." class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            </div>
-            </div>
-            <div class="col-auto mb-3">
-            <div class="card" style={{width: 18+'rem'}}>
-            <img src="..." class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            </div>
-            </div>
-           
+            </div>           
         </div>
     </div>
     </div>
